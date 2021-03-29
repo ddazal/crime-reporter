667bc200455048d2f7f34c4d73a5f65bc859c75d
Ricardo Gamarra <r.richard0000@gmail.com>
2021-03-29 20:17:27 -0300

    Complete Crimes model

--- src/core/crimes/domain/Crime.ts
+++ src/core/crimes/domain/Crime.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, ManyToMany, ObjectID, ObjectIdColumn } from 'typeorm'
+import { Accused } from '../../accused/domain/Accused'
 import { CrimeType } from '../../crime-types/domain/CrimeType'
 
 @Entity('crimes')
@@ -9,15 +10,28 @@ export class Crime {
   @Column()
   description: string
 
+  @Column()
+  occuredDate!: Date
+
+  @Column()
+  lat!: string
+
+  @Column()
+  lng!: string
+
+  @ManyToMany(accuseds => Accused)
+  accuseds: Accused[];
+
   @ManyToMany(types => CrimeType)
   types: CrimeType[];
 
-  private constructor (description: string, types: CrimeType[]) {
+  private constructor (accuseds: Accused[], description: string, types: CrimeType[]) {
+    this.accuseds = accuseds
     this.description = description
     this.types = types
   }
 
-  static create (description: string, types: CrimeType[]) {
-    return new Crime(description, types)
+  static create (accuseds: Accused[], description: string, types: CrimeType[]) {
+    return new Crime(accuseds, description, types)
   }
 }
