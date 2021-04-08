27d6f1f8b25c7076f1a7e570036c57c7128716f0
David Daza <03dazal@gmail.com>
2021-04-08 09:18:01 -0500

    update LoadCrimes migration script

--- migrations/1617880681747-LoadCrimes.ts
+++ migrations/1617880681747-LoadCrimes.ts
@@ -5,21 +5,21 @@ import { AccusedRepository } from '../src/core/accused/infra/AccusedRepository'
 import { CrimeTypeService } from '../src/core/crime-types/application/CrimeTypeService'
 import { CrimeTypeRepository } from '../src/core/crime-types/infra/CrimeTypeRepository'
 import { Crime } from '../src/core/crimes/domain/Crime'
-import lacrimes from '../src/seed/lacrimes.json'
+import crimes from '../src/seed/crimes.json'
 
 export class LoadCrimes1617880681747 implements MigrationInterface {
   public async up (queryRunner: MongoQueryRunner): Promise<void> {
     const accusedService = new AccusedService(new AccusedRepository())
     const typesService = new CrimeTypeService(new CrimeTypeRepository())
 
-    for (const crime of lacrimes) {
+    for (const crime of crimes) {
       const accused = await accusedService.getMany()
       const types = await typesService.getMany()
 
       const row = Crime.create(
         accused,
         crime.description,
-        crime.occurredDate,
+        new Date(crime.occurred_date),
         crime.lat,
         crime.lng,
         types
