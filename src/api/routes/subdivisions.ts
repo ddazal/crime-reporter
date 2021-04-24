a0f0c6b8d0de8d2fe6d50ae6dff30ec2a15b651f
David Daza <03dazal@gmail.com>
2021-04-23 19:56:56 -0500

    add api docs for subdivisions

--- src/api/routes/subdivisions.ts
+++ src/api/routes/subdivisions.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express'
 import { subdivisionService } from '../../core/subdivisions/application'
+import { ApiError } from '../../utils/error'
 
 const router = Router()
 
@@ -16,7 +17,10 @@ router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params
     const subdivision = await subdivisionService.getById(id)
-    res.json({ data: subdivision || null })
+    if (!subdivision) {
+      return next(new ApiError('Subdivision not found', 404))
+    }
+    res.json({ data: subdivision })
   } catch (error) {
     next(error)
   }
